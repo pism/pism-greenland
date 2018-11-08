6724633001d86b923fc503f8f4673806d38c9db6
Andy Aschwanden <aaschwanden@alaska.edu>
2018-11-08 13:48:03 -0900

    FrontalMelt DischargeRouting
    Getting started on testing script

---
+++ test/frontal_melt_routing.py
@@ -0,0 +1,53 @@
+import PISM
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+# set up the option parser
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.description = "Generating scripts for warming experiments."
+parser.add_argument("FILE", nargs=1, help="Bootstrap file", default=None)
+parser.add_argument("--routing_file", dest="routing_file", help="routing file", default="JIB_DMI-HIRHAM5_GL2_ERAI_1980_2014_MRROS_YMM_EPSG3413_3600m_0.nc")
+parser.add_argument("--th_file", dest="th_file", help="Thermal forcing file", default=None)
+
+options = parser.parse_args()
+input_file = options.FILE[0]
+routing_file = options.routing_file
+th_file = options.th_file
+
+context = PISM.Context()
+ctx = context.ctx
+config = context.config
+
+registration = PISM.CELL_CENTER
+
+
+grid = PISM.IceGrid.FromFile(ctx, input_file, ("bed", "thickness"), registration)
+geometry = PISM.Geometry(grid)
+geometry.ice_thickness.regrid(input_file, critical=True)
+geometry.bed_elevation.regrid(input_file, critical=True)
+min_thickness = config.get_double("geometry.ice_free_thickness_standard")
+geometry.ensure_consistency(min_thickness)
+
+potential_temperature = 0.0
+
+if th_file is None:
+    th_file = 'th_given.nc'
+    PISM.util.prepare_output(th_file)
+
+    Th = PISM.IceModelVec2S(grid, "theta_ocean", PISM.WITHOUT_GHOSTS)
+    Th.set_attrs("climate", "potential temperature", "Kelvin", "")
+    Th.set(potential_temperature)
+    Th.write(th_file)
+
+config.set_string("frontal_melt.routing.file", th_file)
+config.set_string("hydrology.surface_input_file", routing_file)
+config.set_double("hydrology.tillwat_max", 0.0)
+
+inputs = PISM.FrontalMeltInputs()
+cell_area = grid.dx() * grid.dy()
+water_density = config.get_double("constants.fresh_water.density")
+
+hydrology = PISM.RoutingHydrology(grid)
+
+frontalmeltmodel = PISM.FrontalMeltDischargeRouting(grid)
+frontalmeltmodel.init(geometry)
+# model.update(inputs, 0, 0.1)
