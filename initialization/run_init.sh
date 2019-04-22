653b98ab1c6625bae7d49d5d316b0f75a38101b0
Andy Aschwanden <aaschwanden@alaska.edu>
2019-04-22 01:06:58 -0800

    prepping to test ISMIP6 flags

---
+++ initialization/run_init.sh
@@ -0,0 +1,3 @@
+python initialization.py -e ctrl --o_dir 2019_04_init -q long -s pleiades_broadwell -w 56:00:00 -n 140  ../../pism-gris/calibration/2017_06_vc/state/gris_g900m_flux_v3a_no_bath_sia_e_1.25_sia_n_3_ssa_n_3.25_ppq_0.6_tefo_0.02_calving_vonmises_calving_0_100.nc
+
+python initialization.py -e asmb --o_dir 2019_04_init -q long -s pleiades_broadwell -w 56:00:00 -n 140  ../../pism-gris/calibration/2017_06_vc/state/gris_g900m_flux_v3a_no_bath_sia_e_1.25_sia_n_3_ssa_n_3.25_ppq_0.6_tefo_0.02_calving_vonmises_calving_0_100.nc 
