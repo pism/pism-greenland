cc9f004d3f22949b52676578dea9d02b0766610e
Andy Aschwanden <aaschwanden@alaska.edu>
2019-06-05 16:47:24 -0800

    adding ISMIP6 vars table for postprocessing

--- initialization/post_initmip.sh
+++ initialization/post_initmip.sh
@@ -16,7 +16,16 @@ start_date=2008-1-1
 scalar_dir=scalar_processed
 mkdir -p ${odir}/${scalar_dir}
 
+# Split variables
 for exp in ctrl asmb; do
-    CDO_FILE_SUFFIX=_${IS}_${GROUP}_${MODEL}_${exp}.nc cdo -f nc4 -z zip_3 splitname -settunits,day -settaxis,${start_date},00:00:00,5year  ${odir}_tmp/ex_ismip6_g1000m_v3a_exp_${exp}_2008-1-1_2108-1-1.nc  ${odir}/spatial/
-    CDO_FILE_SUFFIX=_${IS}_${GROUP}_${MODEL}_${exp}.nc cdo  -f nc4 splitname -settunits,days  -settaxis,${start_date},0:00:00,1year ${odir}/scalar/ts_ismip6_g1000m_v3a_exp_${exp}_2008-1-1_2108-1-1.nc  ${odir}/${scalar_dir}/
+    CDO_FILE_SUFFIX=_${IS}_${GROUP}_${MODEL}_${exp}.nc cdo -f nc4 -z zip_3 splitname  ${odir}_tmp/ex_ismip6_g1000m_v3a_exp_${exp}_2008-1-1_2108-1-1.nc  ${odir}/spatial/
+    CDO_FILE_SUFFIX=_${IS}_${GROUP}_${MODEL}_${exp}.nc cdo  -f nc4 splitname  ${odir}/scalar/ts_ismip6_g1000m_v3a_exp_${exp}_2008-1-1_2108-1-1.nc  ${odir}/${scalar_dir}/
+done
+
+# Adjust time axis
+for file in ${odir}/spatial/*.nc; do
+    ~/pism/util/adjust_timeline.py -i 5 -a ${start_date} -d ${start_date} -p yearly $file
+done
+for file in ${odir}/${scalar_dir}/*.nc; do
+    ~/pism/util/adjust_timeline.py -i 1 -a ${start_date} -d ${start_date} -p yearly $file
 done
