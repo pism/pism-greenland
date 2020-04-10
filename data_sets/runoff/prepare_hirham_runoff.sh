41b4fd8e9ecd9281f58209fa0dc7d26dc0aed687
Andy <andy.aschwanden@gmail.com>
2020-04-10 07:35:15 -0800

    adding scripts to process TSX velocities from Measures

--- data_sets/runoff/prepare_hirham_runoff.sh
+++ data_sets/runoff/prepare_hirham_runoff.sh
@@ -13,18 +13,20 @@ done
 
 for file in "DMI-HIRHAM5_GL2_ERAI_1980_2014_MRROS_DM.nc" "DMI-HIRHAM5_GL2_ERAI_2015_2016_MRROS_DM.nc"; do
     wget -nc ${prefix}/${file}.gz -P ${gz_dir}/
-        gunzip -c ${gz_dir}/${file}.gz > ${extracted_dir}/${file}
+    gunzip -c ${gz_dir}/${file}.gz > ${extracted_dir}/${file}
 done
 
 ofile_dm=DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_EPSG3413_${grid}M_DM.nc
 ofile_mm=DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_EPSG3413_${grid}M_MM.nc
 tmp_file=tmp_${ofile_dm}
+
 #
 # "DMI-HIRHAM5_GL2_ERAI_1980_2014_MRROS_DM.nc" in mm / day with rho = 1000 kg / m3 we get kg / m2 / day
 # "DMI-HIRHAM5_GL2_ERAI_2015_2016_MRROS_DM.nc" claims to be in kg / m2 / s but this doesn't make sense,
-# it's in kg m-2 day-1
-cdo -L -O -f nc4 --reduce_dim sellevel,0 -mergetime -setmisstoc,0 -setattribute,mrros@units="kg m-2 day-1" ${extracted_dir}/DMI-HIRHAM5_GL2_ERAI_1980_2014_MRROS_DM.nc -setattribute,mrros@units="kg m-2 day-1" ${extracted_dir}/DMI-HIRHAM5_GL2_ERAI_2015_2016_MRROS_DM.nc DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_DM.nc
+
+cdo -L -O -f nc4 --reduce_dim sellevel,0 -mergetime -setmisstoc,0 -setattribute,mrros@units="kg m-2 day-1" ${extracted_dir}/DMI-HIRHAM5_GL2_ERAI_1980_2014_MRROS_DM.nc -setattribute,mrros@units="kg m-2 day-1" -muldpm ${extracted_dir}/DMI-HIRHAM5_GL2_ERAI_2015_2016_MRROS_DM.nc DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_DM.nc
 adjust_timeline.py -p daily -a 1980-1-1 -d 1980-1-1 DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_DM.nc
+
 create_greenland_epsg3413_grid.py -g ${grid} $grid_file
 nc2cdo.py $grid_file
 cdo remapbil,${grid_file} -setgrid,../../util/rotated_grid.txt DMI-HIRHAM5_GL2_ERAI_1980_2016_MRROS_DM.nc $tmp_file
---
+++ data_sets/velocities/prepare_velocities.py
@@ -0,0 +1,40 @@
+from netCDF4 import Dataset as NC
+from cdo import Cdo
+from datetime import datetime, timedelta
+import gdal
+from glob import glob
+
+cdo = Cdo()
+reftime = "2008-1-1"
+
+# Let's just process the two velocity compontents:
+components = ["vx", "vy"]
+
+for glacier in ["W69.10N"]:
+    f_tiffs = glob(f"TSX_{glacier}_*_v02.0.tif")
+    
+    for f_tiff in f_tiffs:
+        print(f"Converting {f_tiff} to {f_nc}")
+        f_nc = f_tiff.replace(".tif", ".nc")
+        # use gdal's python binging to convert GeoTiff to netCDF
+        # advantage of GDAL: it gets the projection information right
+        # disadvantage: the variable is named "Band1", lacks metadata
+        ds = gdal.Open(f_tiff)
+        ds = gdal.Translate(f_nc, ds)
+        ds = None
+
+        # This deduces the mid-point (nominal) date from the filename
+        _ , _ , start_date_str, end_date_str, _, var, _ = f_nc.split("_")
+        start_date = datetime.strptime(start_date_str, "%d%b%y")
+        end_date = datetime.strptime(end_date_str, "%d%b%y")
+        nominal_date = start_date + (end_date - start_date) / 2
+        # Set the time axis
+        cdo.settaxis(nominal_date.isoformat(), input=f"""-setreftime,{reftime} -setattribute,{var}@units="m year-1" -chname,Band1,{var} {f_nc}""", output=f"cdo_{f_nc}", options="-f nc4 -z zip_2")
+
+# Merge the indiviual compontents and sort by time using "mergetime"
+for v in components:
+    fs = glob(f"cdo_TSX_{glacier}_*_{v}_v02.0.nc")
+    cdo.mergetime(input=fs, output=f"TSX_{glacier}_{v}_merged.nc", options="-f nc4 -z zip_2")
+
+# Create the final merged file
+cdo.merge(input=[f"TSX_{glacier}_{v}_merged.nc" for v in components], output=f"TSX_{glacier}_2008_2020.nc", options="-f nc4 -z zip_2")
