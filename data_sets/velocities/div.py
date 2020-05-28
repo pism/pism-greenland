5403630aafc12c4b90bc86d43eabd240b6260d1f
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2020-05-28 13:49:56 -0800

    Straight divergence is working; now do div just for undefined points

--- data_sets/velocities/div.py
+++ data_sets/velocities/div.py
@@ -6,23 +6,54 @@ import netCDF4
 right_diff = ((0,1), (-1.,1.))
 center_diff = ((-1,1), (-.5,.5))
 left_diff = ((-1,0), (-1.,1.))
+nan_stencil = ((0,), (np.nan,))
 
-def get_stencil(data, idim, iyx):
-    if iyx[idim]-1 < 0:
+stencil_choice = [
+    center_diff,    # left and right are OK
+    left_diff,      # rightnan
+    right_diff,     # leftnan
+    nan_stencil]    # Both are nan
+
+def get_stencil(data, iyx):
+    """
+    data:
+        The original data for which we're getting a stencil.
+        Assume we're always getting stencil for dimension=0
+    iyx:
+        Index into data where we are looking
+    """
+    # Test for location at edges
+    if iyx[0]-1 < 0:
         return right_diff
-    elif iyx[idim]+1 >= data.shape[idim]:
+    if iyx[0]+1 >= data.shape[0]:
         return left_diff
+
     return center_diff
+
+#    # Test for missing data on one side or the other
+#    leftnan = 2 if np.isnan(data[iyx[0]-1, iyx[1]]) else 0
+#    rightnan = 1 if np.isnan(data[iyx[0]+1, iyx[1]]) else 0
+#    return stencil_choice[leftnan + rightnan]
 
 def d_dx(data, idim,dyx, rows,cols,vals, factor=1.0, rowoffset=0, coloffset=0):
     """Produces a matrix for the del operator"""
     bydx = 1. / dyx[idim]
     data1 = np.reshape(data,-1)
+
+    # Data array, transposed so idim is now dim=0
+    if idim == 0:
+        dataT = data
+    else:
+        dataT = data.transpose()
+
     stride = data.strides[idim] // data.strides[1]
+    iyx = [0,0]
     for iy in range(0,data.shape[0]):
+        iyx[idim]=iy
         for ix in range(0,data.shape[1]):
+            iyx[1-idim]=ix
             ii = iy*data.shape[1] + ix
-            stcoo,stval = get_stencil(data, idim, (iy,ix))
+            stcoo,stval = get_stencil(dataT, iyx)
             for k in range(0,len(stcoo)):
                 jj = ii + stcoo[k]*stride
                 rows.append(ii+rowoffset)
@@ -30,7 +61,10 @@ def d_dx(data, idim,dyx, rows,cols,vals, factor=1.0, rowoffset=0, coloffset=0):
                 vals.append(factor*stval[k]*bydx)
 
 def div(vu1, shape, dyx, factor, rows, cols, vals):
-    """vu1:
+    """Generates a matrix to compute divergence of a vector field.
+    (v and u are stacked)
+
+    vu1:
         1D matrix of U component of velocity, followed by V component
     shape:
         Shape of the original finite difference grid
@@ -42,9 +76,20 @@ def div(vu1, shape, dyx, factor, rows, cols, vals):
 
 
 def main():
+    t = 0    # Time
     with netCDF4.Dataset('outputs/velocity/TSX_W69.10N_2008_2020_pism.nc') as nc:
-        vvel2 = nc.variables['v_ssa_bc'][1]
-        uvel2 = nc.variables['u_ssa_bc'][0]
+        nc_vvel = nc.variables['v_ssa_bc']
+        nc_vvel.set_auto_mask(False)
+        vvel2 = nc_vvel[t,:]
+        vvel2[vvel2 == nc_vvel._FillValue] = np.nan
+
+        nc_uvel = nc.variables['u_ssa_bc']
+        nc_uvel.set_auto_mask(False)    # Don't use masked arrays
+        uvel2 = nc_uvel[t,:]
+        uvel2[uvel2 == nc_uvel._FillValue] = np.nan
+
+        print('Fill Value {}'.format(nc_uvel._FillValue))
+
 
     with netCDF4.Dataset('outputs/bedmachine/W69.10N-thickness.nc') as nc:
         thickness2 = nc.variables['thickness'][:]
@@ -69,14 +114,20 @@ def main():
         shape=(n1, n1*2))
 
     divvel1 = M * vu1
-    divvel2 = np.reshape(divvel1, vvel1.shape)
+    divvel2 = np.reshape(divvel1, vvel2.shape)
 
-    divvel2[np.abs(divvel2)>10000] = 0
+    nan0 = np.isnan(uvel2)
+    nan1 = np.isnan(divvel2)
+    bn2 = np.logical_and(np.logical_not(nan0), nan1)
+
+#    divvel2[np.abs(divvel2)>10000] = 0
 
     with netCDF4.Dataset('x.nc', 'w') as nc:
         nc.createDimension('y', vvel2.shape[0])
         nc.createDimension('x', vvel2.shape[1])
         ncv = nc.createVariable('div', 'd', ('y','x'))
         ncv[:] = divvel2
+        ncv = nc.createVariable('became_nan', 'd', ('y','x'))
+        ncv[:] = bn2
 
 main()
