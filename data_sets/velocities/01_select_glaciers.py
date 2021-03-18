303b7af019908c9c70114bbd798232620e7babf5
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2021-03-18 09:56:08 -0800

    Extracting ItsLive files.

--- data_sets/velocities/01_select_glaciers.py
+++ data_sets/velocities/01_select_glaciers.py
@@ -5,22 +5,108 @@ import re
 import pandas as pd
 import pyproj
 from uafgi import gdalutil,ogrutil,shputil
-from uafgi.nsidc import nsidc0481
-from uafgi import greenland,pdutil
+from uafgi import pdutil
 import shapely
 import shapely.geometry
 from osgeo import ogr,osr
+import uafgi.data
+import uafgi.data.bkm15
+import uafgi.data.cf20
+import uafgi.data.fj
+import uafgi.data.m17
+import uafgi.data.mwp
+import uafgi.data.ns481
+import uafgi.data.ns642
+import uafgi.data.w21
+import uafgi.data.wkt
+from uafgi.data import greenland
+import pandas_ods_reader
+
+"""Determine a set of glaciers for our experiment."""
 
 
+def _csv_to_tuple(val):
+    """Converts key column value from comma-separated format to standard
+    format."""
+
+    if (type(val) == float) or (val is None):
+        return val
+
+    parts = val.split(',')
+    if len(parts) == 1:
+        return parts[0]
+    return tuple(parts)
+
+def read_overrides(overrides_ods, locations_shp, keycols, join_col, map_wkt):
+
+    """Reads an per-project overrides table, ready to use as overrides in
+    joins.  The table is read as a combination of an ODS file and a
+    shapefile containing the locations of termini; with an attribute
+    in the shapefile matching a column in the overrides_ods file.
+
+    The location of the terminus points is returned in the columns:
+        lat, lon: degrees
+        loc: shapely.geometry.Point
+
+    Args:
+        overrides_ods: filename
+            Name of the overrides file (ODS format)
+            Must contain at least column: <join_col>
+
+        locations_shp: filename
+            Name of the shapefile identifying a location point for each glacier.
+            Must contain at least one attribute: <join_col>
+
+        keycols: [str, ...]
+            Names of columns that are keys (in both datasources).
+            Comma-separate them and turn into tuples.
+
+        join_col: str
+            Name of column used to join overrides and locations
+
+    """
+
+    # Manual overrides spreadsheet
+    over = pandas_ods_reader.read_ods(overrides_ods,1)
+    over = over.drop('comment', axis=1)
+    over = over.dropna(how='all')    # Remove blank lines
+
+    # Manual glacier point locations
+    tl = pd.DataFrame(shputil.read(locations_shp, map_wkt))
+
+    # Convert keycols to tuples
+    for df in (over,tl):
+        for col in keycols:
+            df[col] = df[col].map(_csv_to_tuple)
+
+    # Mark to remove any "extra" columns that we didn't actually join with
+    df = pd.merge(over,tl,how='left',on='w21_key', suffixes=(None,'_DELETEME'))
 
 
+    # Move data from the shapefile to override the lon/lat columns
+    df = df.rename(columns={'_shape' : 'loc'})
+    lon = df['_shape0'].map(lambda xy: xy if type(xy)==float else xy.x)
+    lat = df['_shape0'].map(lambda xy: xy if type(xy)==float else xy.x)
+
+    # Merge the shapefile and spreadsheet lat/lon, if available.
+    if 'lon' in df:
+        df['lon'] = df['_shape0'].map(lambda xy: xy if type(xy)==float else xy.x).fillna(df['lon'])
+    if 'lat' in df:
+        df['lat'] = df['_shape0'].map(lambda xy: xy if type(xy)==float else xy.y).fillna(df['lat'])
+
+    # Remove extraneous columns
+    drops = ['_shape0'] + [x for x in df.columns if x.endswith('_DELETEME')]
+    df = df.drop(drops, axis=1)
+    return df
+
+# ------------------------------------------------------------------
 def select_glaciers(includes, w21_blackouts=None):
     """w21_blackouts:
         DataFrame with index matching w21, cols do not matter; rows we do NOT want to select.
     """
 
     # Master set of glaciers from which we will select
-    w21 = greenland.read_w21(greenland.map_wkt)
+    w21 = uafgi.data.w21.read(uafgi.data.wkt.nsidc_ps_north)
     glaciers = w21.df.copy()
 
     # TESTING
@@ -61,7 +147,9 @@ def select_glaciers_main():
     pd.set_option('display.max_columns', None)
 
     # Read user overrides of joins and columns
-    over = greenland.read_overrides('overrides.ods', 'troughs/shp/terminus_locations.shp', ['w21_key', 'bkm15_key'], 'w21_key', greenland.map_wkt)
+    over = read_overrides(
+        'overrides.ods', 'overrides/terminus_locations.shp',
+        ['w21_key', 'bkm15_key'], 'w21_key', uafgi.data.wkt.nsidc_ps_north)
 
     # Set up glacers we DON'T want to select
     blackouts = pd.DataFrame({
@@ -76,14 +164,14 @@ def select_glaciers_main():
     select = select_glaciers(over, blackouts)
 
     # Join with bkm15
-    bkm15 = greenland.read_bkm15(greenland.map_wkt)
+    bkm15 = uafgi.data.bkm15.read(uafgi.data.wkt.nsidc_ps_north)
     match = greenland.match_allnames(select, bkm15)
     select = match.left_join(overrides=over)
 #    print(select.df.columns)
 
 
 
-    select.df = greenland.override_cols(select.df, over, 'w21_key',
+    select.df = pdutil.override_cols(select.df, over, 'w21_key',
         [('bkm15_lat', 'lat', 'lat'),
          ('bkm15_lon', 'lon', 'lon'),
          ('bkm15_loc', 'loc', 'loc')])
@@ -93,51 +181,55 @@ def select_glaciers_main():
 #    print(select.df[['w21_key','bkm15_key']])
 
     # Identify the hand-drawn fjord for each item
-    fj = greenland.read_fj(greenland.map_wkt)
-    match = greenland.match_point_poly(select, 'loc', fj, 'fj_poly')
-    select = match.left_join(right_cols=['fj_poly'])
+    fj = uafgi.data.fj.read(uafgi.data.wkt.nsidc_ps_north)
+    match = pdutil.match_point_poly(select, 'loc', fj, 'fj_poly')
+    select = match.left_join(right_cols=['fj_poly', 'fj_fid'])
 
-#    print(select.df[['w21_key', 'fj_poly', 'grid']])
+    ns481 = uafgi.data.ns481.read(uafgi.data.wkt.nsidc_ps_north)
 
-    ns481 = greenland.read_ns481(greenland.map_wkt)
-
-    match = greenland.match_point_poly(select, 'loc', ns481, 'ns481_poly',
+    match = pdutil.match_point_poly(select, 'loc', ns481, 'ns481_poly',
         left_cols=['fj_poly'], right_cols=['ns481_poly'])
     match.df['fjord_grid_overlap'] = match.df.apply(
             lambda x: 0 if (type(x['ns481_poly'])==float or type(x['fj_poly']) == float)
             else x['ns481_poly'].intersection(x['fj_poly']).area / x['fj_poly'].area,
             axis=1)
 
-    #match.df = pd.merge(match.df, select.df, on='w21_key', how='left')
-    #match.df.sort_values(['w21_ix'])
     match.df.sort_values(['w21_ix'])
 
     select = match.left_join(overrides=over[['w21_key', 'ns481_key']])
-
     select.df = select.df[~select.df['ns481_key'].isna()]
-    #print(sel.df.columns)
-    #print(select.df[['w21_popular_name', 'lat', 'lon', 'w21_coast', 'ns481_grid', 'fj_poly']])
 
 
     # Join with CALFIN dataset high-frequency termini
-    cf20= greenland.read_cf20(greenland.map_wkt)
+    cf20= uafgi.data.cf20.read(uafgi.data.wkt.nsidc_ps_north)
 #    cf20.df.to_csv('cf20.csv')
     print('===================================')
-    match = greenland.match_point_poly(cf20, 'cf20_locs', select, 'fj_poly').swap()
+    match = pdutil.match_point_poly(cf20, 'cf20_locs', select, 'fj_poly').swap()
     select = match.left_join(overrides=over)
 
     select.df.to_csv('select.csv')
 
     # ----- Join with NSIDC-0642 (MEASURES) annual termini
-    ns642 = greenland.read_ns642(greenland.map_wkt)
+    ns642 = uafgi.data.ns642.read(uafgi.data.wkt.nsidc_ps_north)
     # Combine all points for each GlacierID
-    ns642x = greenland.ns642_by_glacier_id(ns642)
+    ns642x = uafgi.data.ns642.by_glacier_id(ns642)
 
-    match = greenland.match_point_poly(
+    match = pdutil.match_point_poly(
         ns642x, 'ns642_points', select, 'fj_poly',
         right_cols=['lat','lon','w21_key']).swap()
 #    print('xyz1', match.df.columns)
     select = match.left_join(overrides=over)
+
+
+    # ----- Add a single upstream point for each glacier
+    up = shputil.read_df(
+        uafgi.data.join('upstream/upstream_points.shp'),
+        uafgi.data.wkt.nsidc_ps_north, shape='loc', add_prefix='up_')
+
+    match = pdutil.match_point_poly(up, 'up_loc', select, 'fj_poly').swap()
+    select = match.left_join()
+
+        
 
     
 #    print(select.df[['w21_popular_name', 'w21_coast', 'ns481_grid', 'cf20_key', 'ns642_key']])
@@ -155,8 +247,8 @@ def select_glaciers_main():
 
     seldf = select.df.drop(['cf20_locs', 'ns642_points', 'ns481_poly'], axis=1)
     
-    seldf.to_pickle('select.df')
-    seldf.to_csv('select.csv')
+    seldf.to_pickle('select_01.df')
+    seldf.to_csv('select_01.csv')
 
 
     return select
---
+++ data_sets/velocities/02_extract_bedmachine.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+from uafgi import gdalutil,bedmachine,glacier,make
+import uafgi.wkt
+import uafgi.data
+import uafgi.data.ns642
+import uafgi.data.itslive
+
+"""Set up bedmachine files --- both global and local --- as required
+for our experiment."""
+
+
+def bedmachine_global_rule():
+    """Creates a compressed global BedMachine file, suitable for use with PISM"""
+    def action(tdir):
+        from uafgi import bedmachine
+        import uafgi.data
+
+        bedmachine.fixup_for_pism(uafgi.data.BEDMACHINE_ORIG, uafgi.data.BEDMACHINE_PISM, tdir)
+
+    return make.Rule(
+        action,
+        [uafgi.data.BEDMACHINE_ORIG],
+        [uafgi.data.BEDMACHINE_PISM])
+
+def bedmachine_local_rule(ns481_grid):
+    """Creates a localized BedMachine file for a MEASURES grid"""
+    ifname = uafgi.data.measures_grid_file(ns481_grid)
+    ofname = uafgi.data.bedmachine_local(ns481_grid)
+
+    def action(tdir):
+        from uafgi import cdoutil
+        import uafgi.data
+
+        cdoutil.extract_region(
+            uafgi.data.BEDMACHINE_PISM, ifname,
+            ['thickness', 'bed'],
+            ofname, tdir)
+
+    return make.Rule(
+        action,
+        [uafgi.data.BEDMACHINE_PISM, uafgi.data.measures_grid_file(ns481_grid)],
+        [ofname])
+
+# -------------------------------------------------------------
+def itslive_local_rule(ns481_grid):
+    """Creates a localized ItsLive file"""
+
+# -------------------------------------------------------------
+def render_bedmachine_makefile(select):
+    """Given a Glacier selection, creates a Makefile to create all the
+    localized BedMachine files required for it."""
+
+    makefile = make.Makefile()
+
+    # Make the global BedMachine file (compressed, for PISM)
+    makefile.add(bedmachine_global_rule())
+
+    # Make the localized BedMachine extracts
+    targets = list()
+    for grid in select['ns481_grid']:
+        rule = bedmachine_local_rule(grid)
+        makefile.add(rule)
+        targets.append(rule.outputs[0])
+
+    for grid in select['ns481_grid']:
+        print('grid ',grid)
+        rule = uafgi.data.itslive.merge_to_pism_rule(grid,
+            uafgi.data.measures_grid_file(grid),
+            uafgi.data.join('itslive/GRE_G0240_{}.nc'),
+            range(2011,2019), uafgi.data.join_outputs('itslive'))
+        makefile.add(rule)
+        targets.append(rule.outputs[0])
+
+
+    makefile.generate(targets, 'localize_bedmachine.mk')
+
+def main():
+    select = pd.read_pickle('select.df')
+    render_bedmachine_makefile(select)
+    print('Finished rendering Makefile.\n    Run with ./localize_bedmachine.mk/domake')
+
+main()
