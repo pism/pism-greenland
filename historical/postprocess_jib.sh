d1ad4e8ea6bf598b8f69a74de92112329e598e30
Andy Aschwanden <aaschwanden@alaska.edu>
2021-06-22 07:36:57 -0800

    work on postprocessing script

--- historical/postprocess_jib.sh
+++ historical/postprocess_jib.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=analysis
 #SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=pism.%j
 #SBATCH --mem=214G
 
@@ -10,17 +10,20 @@ cd $SLURM_SUBMIT_DIR
 
 odir=$1
 file=$2
+grid=$3
 
 mkdir -p ${odir}/io
+mkdir -p ${odir}/profiles
 
 # Spatial files
 cd  ${odir}/spatial/
 
-cdo -f nc4 -z zip_2 ifnotthen ../../../data_sets/basin_masks/ugid_225_Jakobshavn_Isbrae_mask_epsg3413_g600m.nc ex_$file masked_ex_$file
+cdo -f nc4 -z zip_2 ifnotthen ../../../data_sets/basin_masks/ugid_225_Jakobshavn_Isbrae_mask_epsg3413_g${grid}m.nc ex_$file masked_ex_$file
 cdo fldsum  masked_$file fldsum_masked_$file
 cdo fldmean masked_$file mean_masked_$file
 
 extract_interface.py -t ice_ocean -o ../io/io_masked_ex_$file masked_ex_$file
+extract_profiles.py -v velsurf_mag --srs epsg:3413 /import/c1/ICESHEET/ICESHEET/crios2pism/data_sets/shape_files/joughin-gps-points.shp ex_$file ../profiles/gps_stations_ex_$file
 
 # State files
 cd ../state
