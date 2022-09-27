85089a0d757913558041fc0127457d7eb9a82988
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-09-27 06:37:22 -0800

    added RAGIS UQ

--- uncertainty_quantification/draw_ragis_samples.py
+++ uncertainty_quantification/draw_ragis_samples.py
@@ -41,10 +41,30 @@ dists = {
             "fractures": "false",
         },
     },
+    "ocean": {
+        "uq": {
+            "vcm": uniform(loc=0.25, scale=0.25),
+            "gamma": uniform(loc=1e-4, scale=0.5e-4),
+            "thickness_calving_threshold": uniform(loc=100, scale=300),
+        },
+        "default_values": {
+            "climate": "given",
+            "hydrology": "routing",
+            "frontal_melt": "discharge_routing",
+            "ocean_file": "MAR3.9_MIROC-ESM-CHEM_rcp85_ocean_1960-2100_v4.nc",
+            "climate_file": "DMI-HIRHAM5_ERA_1980_2020_EPSG3413_4500M_MM.nc",
+            "runoff_file": "DMI-HIRHAM5_ERA_1980_2020_EPSG3413_4500M_MM.nc",
+            "salinity": 34,
+            "pseudo_plastic_q": 0.6,
+            "sia_e": 1.25,
+            "ssa_n": 3.0,
+            "fractures": "false",
+        },
+    },
 }
 
 parser = ArgumentParser()
-parser.description = "Draw samples using the Saltelli methods"
+parser.description = "Generate UQ using Latin Hypercube or Sobol Sequences."
 parser.add_argument(
     "-s",
     "--n_samples",
