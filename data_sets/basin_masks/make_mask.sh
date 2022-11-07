c1fbee3135e2a908e2b5062ef5c53f0af497ee23
Andy Aschwanden <aaschwanden@alaska.edu>
2022-11-07 07:29:17 -0900

    duck punched mask

--- data_sets/basin_masks/make_mask.sh
+++ data_sets/basin_masks/make_mask.sh
@@ -9,9 +9,9 @@ y_min=-2417600
 y_max=-2021600
 
 x_min=$((${x_min} - $ores * 2))
-x_max=$((${x_max} + $ores * 2))
+x_max=$((${x_max} + $ores * 1 ))
 y_min=$((${y_min} - $ores * 2))
-y_max=$((${y_max} + $ores * 2))
+y_max=$((${y_max} + $ores * 1 ))
 
 
 gdal_rasterize  -where "UGID='${UGID}'" -l Greenland_Basins_PS_v1.4.2_1980_epsg3413 -tr $ores $ores -a_nodata -9999 -burn 0 -te $x_min $y_min $x_max $y_max -ot Byte ~/base/gris-analysis/basins/Greenland_Basins_PS_v1.4.2_1980_epsg3413.shp ugid_225_Jakobshavn_Isbrae_mask_epsg3413_g${ores}m.nc
