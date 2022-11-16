f8e803a0559d018e558f3a56ddf4cd30d26a0d7e
Andreas Aschwanden <aaschwa@login4.stampede2.tacc.utexas.edu>
2022-11-15 18:29:38 -0600

    Fixed merge conflicts

--- ragis/run_ragis.sh
+++ ragis/run_ragis.sh
@@ -6,10 +6,16 @@ PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --
 
 n=68
 grid=1800
+odir=2022_08_init
+
+PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep yearly --tsstep daily -b rm --dataset_version 1_RAGIS -d gris --o_dir $SCRATCH/${odir} --start 1970-1-1 --end 1980-1-1 -q normal -s stampede2 -w 4:00:00 -n 68 -g ${grid} -e $WORK/crios2pism/uncertainty_quantification/ensemble_gris_ragis_calving_lhs_20_w_posterior.csv $WORK/crios2pism/data_sets/initial_states/gris_g${grid}m_v1_RAGIS_id_0_0_50.nc
+
+n=68
+grid=1800
 odir=2022_08_calving
 
 for id in {0..19}; do
-PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep monthly --tsstep daily -b rm --dataset_version 1_RAGIS -d gris --o_dir $SCRATCH/${odir} --start 1980-1-1 --end 2020-1-1 -q normal -s stampede2 -w 8:00:00 -n 68 -g ${grid} -e $WORK/crios2pism/uncertainty_quantification/ensemble_gris_ragis_calving_lhs_20_w_posterior.csv $SCRATCH/2022_08_init/state/gris_g1800m_v1_RAGIS_id_${id}_1980-1-1_1990-1-1.nc
+PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep monthly --tsstep daily -b rm --dataset_version 1_RAGIS -d gris --o_dir $SCRATCH/${odir} --start 1980-1-1 --end 2020-1-1 -q normal -s stampede2 -w 12:00:00 -n 68 -g ${grid} -e $WORK/crios2pism/uncertainty_quantification/ensemble_gris_ragis_calving_lhs_20_w_posterior.csv $SCRATCH/2022_08_init/state/gris_g1800m_v1_RAGIS_id_${id}_1970-1-1_1980-1-1.nc
 
 sbatch $SCRATCH/${odir}/run_scripts/gris_g1800m_v1_RAGIS_id_${id}_1980-1-1_2020-1-1.sh
 done
@@ -20,6 +26,17 @@ odir=2022_09_init
 uq=gris_ragis_init
 PISM_PREFIX=$HOME/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep monthly --tsstep daily  -d gris --o_dir ${odir} --start 1980-1-1 --end 1990-1-1 -q normal -s pleiades_ivy -w 6:00:00 -n $n -g ${grid} -e ../uncertainty_quantification/ensemble_${uq}.csv ../data_sets/initial_states/gris_g${grid}m_v5_RAGIS_id_0_0_50.nc
 
+n=68
+grid=1800
+odir=2022_10_ocean
+uq=gris_ragis_ocean_lhs_30_w_posterior
+PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep monthly --tsstep daily  -d gris --o_dir $SCRATCH/${odir} --start 1980-1-1 --end 2020-1-1 -q normal -s stampede2 -w 12:00:00 -n 68 -g ${grid} -e $WORK/crios2pism/uncertainty_quantification/ensemble_${uq}.csv $SCRATCH/2022_10_init/state/gris_g1800m_v5_RAGIS_id_0_1980-1-1_1990-1-1.nc
+
+n=68
+grid=1800
+odir=2022_10_dem
+uq=gris_ragis_ocean_lhs_30_w_posterior
+PISM_PREFIX=$WORK/local/pism-dev/bin/ python3 hindcast.py --hydrology routing --spatial_ts standard --exstep monthly --tsstep daily  -d gris --o_dir $SCRATCH/${odir} --start 1975-1-1 --end 1980-1-1 -q normal -s stampede2 -w 4:00:00 -n 68 -g ${grid} -e $WORK/crios2pism/uncertainty_quantification/ensemble_${uq}.csv $SCRATCH/2022_10_init/state/gris_g1800m_v5_RAGIS_id_0_1980-1-1_1990-1-1.nc
 
 
 uq=gris_ragis_dem_lhs_20_w_posterior
