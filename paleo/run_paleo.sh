46e613611390e3712e64c8a9af2318cec156ed07
Andy Aschwanden <aaschwanden@alaska.edu>
2023-10-04 16:27:19 -0800

    improving paleo ensemble analysis

--- paleo/run_paleo.sh
+++ paleo/run_paleo.sh
@@ -1,4 +1,5 @@
-PISM_PREFIX=/Users/andy/pism python initialize_lgm.py -e ../uncertainty_quantification/ensemble_gris_lgm_init.csv -g 18000 -s debug -q debug --start -145001 --end -20000 --exstep 10000 --calving float_kill -n 4 --stress_balance sia --bed_def off --o_dir 2023_09_init_nomass --o_format netcdf4_serial --compression_level 2
+PISM_PREFIX=$HOME/local-rl8/pism python run_glacial_cycle.py -w 72:00:00 -e ../uncertainty_quantification/ensemble_gris_paleo_climate-calving_w_posterior_lhs_20.csv -g 12000 -s chinook-rl8 -q t2small --exstep 250 --calving hybrid_calving -n 20 --stress_balance ssa+sia --age --bed_def lc --o_dir 2023_10_climate_calving_w_posterior --o_format netcdf4_parallel --compression_level 2 -i ../../best_v1/g4500m_const_ctrl_e_1.25_ppq_0.6_tefo_0.02_ssa_n_3.25_philow_5.0_hydro_null_100a.nc
+
 
 
 
